3a3ae6bc48901b968debc4d8829024dc7ca6c937
Kate-Kitkat19 <katekateee1911@gmail.com>
2022-11-23 20:45:26 +0200

    markup task 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import user from './Profile/user.json';
 import data from './Statistics/data.json';
 import friends from './FriendList/friends.json';
+import transactions from './Transactions/transactions.json';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendsList } from './FriendList/FriendList';
+import { TransactionHistory } from './Transactions/TransactionHistory';
 
-console.log('data', data);
-
+console.log(transactions);
 export const App = () => {
   return (
     <>
@@ -19,6 +20,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendsList friends={friends} />
+      <TransactionHistory items={transactions}></TransactionHistory>
     </>
   );
 };
